5fb36ac9fe54f2bb0e61888ba11c731ef6e0edfc
Avery <avemoor01@gmail.com>
2024-08-06 22:09:42 -0400

    Initial Commit

---
+++ canvas.gd
@@ -0,0 +1,14 @@
+extends Node2D
+var angle = 0
+var increase = 0.01
+var materials
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	materials = self.material
+
+func _physics_process(delta):
+	angle += increase
+	materials.set_shader_parameter("angle", angle)
+
+func _draw():
+	draw_rect(Rect2(Vector2(0, 0), Vector2(640, 360)), Color.WHITE)
